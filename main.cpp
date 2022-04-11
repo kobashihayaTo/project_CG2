ca4fd9c343c10633dd2f7c6e09a5980e42e5854a
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-04-11 15:53:04 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <Windows.h>
+//ウィンドウプロシージャー
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam){
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+	//ウィンドウが破壊された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+
+	}
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//コンソールへの文字出力
+	OutputDebugStringA("Hello,DirectX!!\n");
+
+	//ウィンドウサイズ
+	const int window_width = 1280;
+	const int window_height = 720;
+
+	//ウィンドウクラスの設定
+	WNDCLASSEX w{};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;
+	w.lpszClassName = L"DirectXGame";
+	w.hInstance = GetModuleHandle(nullptr);
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	//ウィンドウクラスをOSに登録
+	RegisterClassEx(&w);
+	//ウィンドウサイズ{X,Y,横,縦}
+	RECT wrc = { 0,0,window_width,window_height };
+	//自動でサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	
+	//ウィンドウプロジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,
+		L"DirectXGame",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr);
+	//ウィンドウを表示状態にする
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+
+
+	//ゲームループ
+	while (true) {
+		//メッセージがある？
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);//キー入力メッセージの処理
+			DispatchMessage(&msg);//プロシージャにメッセージを送る
+		}
+
+		//×ボタンで終了メッセージが来たらゲームループを抜ける
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+	}
+	//ウィンドウクラスを登録解除
+	UnregisterClass(w.lpszClassName, w.hInstance);
+	return 0;
+}
